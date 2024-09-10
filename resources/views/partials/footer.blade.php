17339126df8a5497c3bc45ba2dbd5e54ae156790
FraCuboni <fracuboni@gmail.com>
2024-09-10 15:55:19 +0200

    feat started with footer and header

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,65 @@
-<footer class="my-5 text-center">
-    <p>by UdU</p>
-</footer>
+<div class="container footer-top">
+
+    <div class="box-1">
+            <ul>
+                <li><h3>nome lista</h3></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+            </ul>
+            <ul>
+                <li><h3>nome lista</h3></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+            </ul>
+            <ul>
+                <li><h3>nome lista</h3></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+            </ul>
+            <ul>
+                <li><h3>nome lista</h3></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+                <li><p>link</p></li>
+            </ul>
+        
+    </div>
+
+    <div class="box-2">
+        <div>
+            <img src="" alt="">
+        </div>
+    </div>
+
+</div>
---
+++ resources/views/partials/footerb.blade.php
@@ -0,0 +1,30 @@
+<div class="container footer-bottom">
+
+
+    <div class="box">
+        <button>SIGN-UP NOW!</button>
+    </div>
+
+    <div class="box">
+        <ul>
+            <li>
+                <p>follow us</p>
+            </li>
+            <li class="logo">
+                <img src="../assets/footer-facebook.png" alt="">
+            </li>
+            <li class="logo">
+                <img src="../assets/footer-pinterest.png" alt="">
+            </li>
+            <li class="logo">
+                <img src="../assets/footer-periscope.png" alt="">
+            </li>
+            <li class="logo">
+                <img src="../assets/footer-twitter.png" alt="">
+            </li>
+            <li class="logo">
+                <img src="../assets/footer-youtube.png" alt="">
+            </li>
+        </ul>
+    </div>
+</div>
